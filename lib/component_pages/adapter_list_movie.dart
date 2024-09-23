5bbb1c5f7f553ab8b531bde48fe0b9d28b1cc96e
Andhika Eka Santosa <123168811+andhikaeka3333333@users.noreply.github.com>
2024-09-23 11:00:00 +0700

    Feat:[AE]create popular menu

---
+++ lib/component_pages/adapter_list_movie.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../model/model_movie.dart';
+import '../widgets/colors.dart';
+import '../widgets/item_movie.dart';
+
+class AdapterListMovie extends StatelessWidget {
+  final ModelMovie modelMovie;
+
+  const AdapterListMovie({super.key, required this.modelMovie});
+
+  @override
+  Widget build(BuildContext context) {
+    return ItemMovie(
+      containerMargin: 15.0,
+      containerHeight: 180.0,
+      containerColor: containerColor,
+      containerRadius: 15.0,
+      padding: 8.0,
+      imageWidth: 85.0,
+      imageHeight: 127.0,
+      imageRadius: 8.0,
+      image: modelMovie.image,
+      imageFit: BoxFit.cover,
+      marginTop: 20.0,
+      marginLeft: 12.0,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      title: modelMovie.title,
+      titleFontSize: 16.0,
+      titleFontWeight: FontWeight.bold,
+      font: 'Mulish',
+      marginRating: 10.0,
+      ratingFontSize: 14.0,
+      rating: modelMovie.rating,
+      iconSize: 15.0,
+      ratingIconColor: primaryColor,
+      genre: modelMovie.genre,
+      genreTextColor: buttonTextColor,
+      genreFontSize: 10.0,
+      genreFontWeight: FontWeight.bold,
+      genreBackgroundColor: buttonBackgroundColor,
+      genreWidth: 75.0,
+      genreHeight: 24.0,
+      genreHorizontalPadding: 12.0,
+      genreVerticalPadding: 4.0,
+      genreRadius: 20.0,
+      durationIcon: Icons.access_time,
+      duration: modelMovie.duration,
+      spaceDuration: 5.0,
+      onTap: () {
+        Get.toNamed('/movie-detail', arguments: modelMovie);
+      },
+    );
+  }
+}
