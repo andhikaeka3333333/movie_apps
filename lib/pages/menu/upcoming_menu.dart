c194b069db73bb40aafa9ed645e4cd83a9564e28
Andhika Eka Santosa <123168811+andhikaeka3333333@users.noreply.github.com>
2024-09-23 11:09:30 +0700

    Feat:[AE]create upcoming menu

---
+++ lib/pages/menu/upcoming_menu.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UpcomingMenu extends StatelessWidget {
+  const UpcomingMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Upcoming"),
+      ),
+    );
+  }
+}
