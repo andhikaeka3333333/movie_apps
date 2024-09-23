ae8147fe085d5ef3862bc19d092e83299ea7af08
Andhika Eka Santosa <123168811+andhikaeka3333333@users.noreply.github.com>
2024-09-23 10:42:26 +0700

    Feat:[AE]add colors

--- lib/widgets/colors.dart
+++ lib/widgets/colors.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
-
 final Color primaryColor = Color(0xffffc319);
-final Color secondaryColor =  Color(0xff9c9c9c);
+final Color secondaryColor = Color(0xff9c9c9c);
 final Color containerColor = Color(0xfff6f4f0);
 final Color containerColorHorizontal = Color(0xfffdd86e);
 final Color buttonTextColor = Color(0xff88a4e8);
 final Color buttonBackgroundColor = Color(0xffdbe3ff);
+final Color colorTransparent = Colors.transparent;
+final Color backgroundSearchBar = Color(0xffdad8d8);
 final Color blackColor = Colors.black;
-
+final Color whiteColor = Colors.white;
