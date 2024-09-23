4f50ffafe42971f5f26a54463efe04b505d229d3
Andhika Eka Santosa <123168811+andhikaeka3333333@users.noreply.github.com>
2024-09-23 10:55:45 +0700

    Feat:[AE]create model

---
+++ lib/model/model_movie.dart
@@ -0,0 +1,12 @@
+class ModelMovie {
+  final String title;
+  final String rating;
+  final String genre;
+  final String duration;
+  final String image;
+  final String description;
+  final String imageHorizontal;
+
+  ModelMovie(this.title, this.rating, this.genre, this.duration, this.image,
+      this.description, this.imageHorizontal);
+}
